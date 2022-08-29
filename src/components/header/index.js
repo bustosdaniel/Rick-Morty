9946ed13020077a8a787a883db4a7c788ac58eac
Daniel <bustosneifer@gmail.com>
2022-08-29 16:57:23 -0500

    added header

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../header/index.css'
+
+export default function Header() {
+  return (
+    <header className='header-app'>
+      <h1>
+        The Rick and Morty API
+      </h1>
+    </header>
+  )
+}
